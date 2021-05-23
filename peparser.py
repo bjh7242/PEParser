767e2451ec426d19fe8f542826b412a7cfa35252
bjh7242 <bjh7242@rit.edu>
2021-05-23 18:01:02 -0400

    adding more stuff

--- peparser.py
+++ peparser.py
@@ -14,14 +14,21 @@ def parse_pe(filename):
         print("[-] PEFormatError: %s" % e.value)
         sys.exit()
     
-    import pdb; pdb.set_trace()
-
     attributes = {'IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE': pe.OPTIONAL_HEADER.IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE,
                   'IMAGE_DLLCHARACTERISTICS_GUARD_CF': pe.OPTIONAL_HEADER.IMAGE_DLLCHARACTERISTICS_GUARD_CF,
                   'IMAGE_DLLCHARACTERISTICS_NX_COMPAT': pe.OPTIONAL_HEADER.IMAGE_DLLCHARACTERISTICS_NX_COMPAT,
                   'IMAGE_DLLCHARACTERISTICS_NO_SEH': pe.OPTIONAL_HEADER.IMAGE_DLLCHARACTERISTICS_NO_SEH}
 
-    print("===== Attributes =====")
+    #import pdb; pdb.set_trace()
+
+    print("===== %s Attributes =====" % filename)
+
+    # ref: https://docs.microsoft.com/en-us/windows/win32/debug/pe-format?redirectedfrom=MSDN
+    if pe.FILE_HEADER.IMAGE_FILE_32BIT_MACHINE:
+        print("\t*** x86 Binary ***")
+    elif pe.FILE_HEADER.Machine == 0x8664:		# x64 binary
+        print("\t*** x64 Binary ***")
+
     for attr in attributes.keys():
         if attributes[attr] == True:
             print("\t%s: %s" % (attr, colored(attributes[attr], 'red')))
@@ -35,7 +42,7 @@ def parse_pe(filename):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', action='store', help="Filename of PE file to parse", required=True)
+    parser.add_argument('-f', '--filename', action='store', help="Filename of PE file to parse", required=True)
     parser.add_argument('-o', '--optional-header', action='store_true', help="Print the optional header")
     args = parser.parse_args()
-    parse_pe(args.f)
+    parse_pe(args.filename)
